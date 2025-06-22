cd3cd50d6605d3758b27bb19c679dce87437eacc
A N O N Y M <157827248+A-N-O-N-Y-M@users.noreply.github.com>
2025-06-22 14:57:55 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-TG_API_TOKEN = '7842333070:AAHRusFfbRmZnNtTet7OB_IswgPrkhb9G98'
-
-DB_CONFIG = {
-    'host': 'FVH1.spaceweb.ru',
-    'port': 3306,
-    'user': 'igoryanpid',
-    'password': 'D1GEY#982bHHPQW8',
-    'db': 'igoryanpid',
-}
